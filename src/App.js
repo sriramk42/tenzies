e9e26d78e08a12a6edf51118a94eaa812bc2b9a0
Sriram Krishnan <sriramk1986@gmail.com>
2022-07-29 21:53:22 +0530

    finished game

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import Die from "./Die";
 import { nanoid } from 'nanoid' 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
 
 function App() {
   const [dice, setDice] = useState(setNewDice())
+  const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    const firstValue = dice[0].value
+    const sameValue = dice.every(die => die.value === firstValue)
+    const allFixed = dice.every(die => die.isFixed)
+    if (sameValue && allFixed) {
+      setTenzies(true)
+    }  
+  }, [dice])
 
   function updateFixed(id) {
-    console.log(id)
+    setDice(prevDice => prevDice.map(die => {
+     return die.id === id ?
+        {...die, isFixed: !die.isFixed} : die
+    }))
   }
+
+  function rollDice() {
+    if (!tenzies) {
+      setDice(prevDice => prevDice.map(die => {
+        return die.isFixed ? die : {
+          value: Math.ceil(Math.random() * 6),
+          isFixed: false,
+          id: nanoid()
+        }
+      }))
+    } else {
+      setTenzies(false)
+      setDice(setNewDice())
+    }
+  } 
 
   function setNewDice() {
     const newDice = []
@@ -30,6 +59,7 @@ function App() {
   return (
     <div className="App">
       <main>
+        {tenzies && <Confetti />}
         <h1>Tenzies</h1>
         <p>Roll until all dice are the same.</p>
         <p>Click each die to freeze it at its current value between rolls</p>
@@ -38,7 +68,9 @@ function App() {
           {diceElements}
         </div>
 
-        <button onClick={setNewDice}>Roll Dice</button>
+        <button onClick={rollDice}>
+          {tenzies ? "Reset Game" : "Roll Dice"}
+        </button>
       </main>
     </div>
   );
